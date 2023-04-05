f826a0df100aecb2ab8c76eb9dc96424458ec72c
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-05 15:26:21 -0600

    Add WIP script to move all files to user_facing_app package

---
+++ 02_create_user_app_package.sh
@@ -0,0 +1,39 @@
+
+set -e
+
+if [ "$1" = "ni" ]; then
+    interactive=false
+else
+    interactive=true
+fi
+
+$interactive || echo "Script in non-interactive mode"
+$interactive && echo "Script in interactive mode"
+
+
+
+
+
+echo "\n\n\n################################################################################"
+echo "################################################################################"
+echo "################################################################################"
+echo "\nSCRIPT 02: Move all code into a user faching app package"
+$interactive && read -n 1 -p "Press any key to continue"
+echo ""
+
+bin/packs create packs/user_facing_app
+
+bin/packs move packs/user_facing_app app
+
+bin/packwerk update
+
+find . -iname "account_actions_controller_spec.rb" | xargs rspec spec/features
+
+
+
+
+
+
+echo "If there are no more package_todo.yml files, we're good"
+
+find . -iname "package_todo.yml"
