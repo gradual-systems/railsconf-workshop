795b1b8114b422db9cbfcba57fbe0bd6a814e918
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-04-06 09:52:54 -0400

    Various changes to 00 script

--- 00_mastodon_test.sh
+++ 00_mastodon_test.sh
@@ -21,14 +21,19 @@ echo "Also, verifying that the mastodon specs can run successfully"
 $interactive && read -n 1 -p "Press any key to continue"
 echo ""
 
+echo "Using rbenv to set ruby version to 3.2.1"
+rbenv global 3.2.1
+
+echo "Installing system dependencies"
 brew install gnu-sed libidn libpq postgresql redis yarn ffmpeg imagemagick
 
-
+echo "Git checckout, clean, and pull"
 git checkout .
 git clean -fd
 
 git pull --rebase
 
+echo "Installing bundle"
 bundle
 
 bundle binstubs bundler --force
@@ -36,13 +41,12 @@ bundle binstubs bundler --force
 # nvm use 19
 
 # From https://github.com/mastodon/mastodon/blob/main/.devcontainer/post-create.sh
+# export NODE_OPTIONS=--openssl-legacy-provider
 
-export NODE_OPTIONS=--openssl-legacy-provider
+echo "Fetch Javascript dependencies"
+yarn --frozen-lockfile
 
-# Fetch Javascript dependencies
-yarn install
-
-# Make Gemfile.lock pristine again
+echo "Make Gemfile.lock pristine again"
 git checkout -- Gemfile.lock
 
 RAILS_ENV=test ./bin/rails db:drop
@@ -62,4 +66,4 @@ RAILS_ENV=development ./bin/rails assets:precompile
 # RAILS_ENV=test NODE_ENV=tests ./bin/rails assets:precompile
 cp public/packs/manifest.json public/packs-test/manifest.json 
 
-find . -iname "account_actions_controller_spec.rb" | xargs rspec spec/features
+find . -iname "account_actions_controller_spec.rb" | xargs bundle exec rspec spec/features
