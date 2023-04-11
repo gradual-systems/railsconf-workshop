5c1310abd988b88cc924fc6781d02137fd592f85
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-11 17:17:12 -0600

    Split script according to new order

--- 01_create_admin_package.sh
+++ 01_create_admin_package.sh
@@ -47,6 +47,7 @@ gsed '/use_packs/d' Gemfile
 
 echo "
 gem 'use_packs'
+gem 'packs-rails'
 gem 'visualize_packwerk', path: '../visualize_packwerk'
 " >> Gemfile
 
@@ -54,6 +55,13 @@ echo "pack_paths:
   - packs/*
   - .
 " > packs.yml
+
+echo "
+--require packs/rails/rspec
+--color
+--require spec_helper
+--format Fuubar
+" > .rspec
 
 bundle
 
@@ -85,29 +93,6 @@ bin/packs move packs/admin \
   app/views/admin \
   app/workers/admin
 
-find . -iname "account_actions_controller_spec.rb" | xargs rspec spec/features || puts_h2 "Spec execution failed because packs directory is being ignored by Rails"
-
-
-
-
-
-puts_h2 "Installing packs-rails to make Rails pick up the packs directory"
-$interactive && read -n 1 -p "Press any key to continue"
-puts ""
-
-echo "
-gem 'packs-rails'
-" >> Gemfile
-
-bundle
-
-echo "
---require packs/rails/rspec
---color
---require spec_helper
---format Fuubar
-" > .rspec
-
 find . -iname "account_actions_controller_spec.rb" | xargs rspec spec/features
 
 
@@ -133,74 +118,3 @@ puts_h2 "Check out package_todo.yml and packs/admin/package_todo.yml"
 puts "Not many violations in the root, YIKES!! Lots of violations in the admin... which is expected"
 $interactive && read -n 1 -p "Press any key to continue"
 puts ""
-
-
-
-
-
-
-puts_h2 "Let's accept that the admin package depends on the root package"
-$interactive && read -n 1 -p "Press any key to continue"
-puts ""
-
-echo "
-dependencies:
-- .
-" >> packs/admin/package.yml
-
-bin/packwerk update
-
-echo 'VisualizePackwerk.package_graph!(Packs.all)' | RAILS_ENV=development bundle exec rails c
-mv packwerk.png ../01-02_admin_depending_on_root.png
-find . -name "package_todo.yml" -exec basename {} \; -exec cat {} \; > ../01-02_admin_depending_on_root_package_todo.yml
-$interactive && open ../01-02_admin_depending_on_root.png
-
-
-
-
-puts_h2 "Let's take a closer look at the root package_todo.yml"
-$interactive && read -n 1 -p "Press any key to continue"
-puts ""
-
-$interactive && open package_todo.yml
-
-
-
-
-
-puts_h2 "There is a bunch of admin code still in the root pack (In those API folders)"
-puts_h1 "Move the API admin code into the admin package and update package todos"
-$interactive && read -n 1 -p "Press any key to continue"
-puts ""
-
-bin/packs move packs/admin \
-  app/controllers/api/v1/admin \
-  app/controllers/api/v2/admin \
-
-bin/packwerk update
-
-
-
-
-
-puts_h2 "Let's see if can't move the remaining violation causing files into a pack and avoid violations..."
-$interactive && read -n 1 -p "Press any key to continue"
-puts ""
-
-bin/packs create packs/messy_middle
-
-bin/packs move packs/messy_middle \
-  packs/admin/app/workers/admin/account_deletion_worker.rb \
-  packs/admin/app/models/admin/action_log.rb \
-  packs/admin/app/models/admin/import.rb \
-  packs/admin/app/workers/admin/suspension_worker.rb \
-  packs/admin/app/workers/admin/unsuspension_worker.rb
-
-bin/packwerk update
-
-echo 'VisualizePackwerk.package_graph!(Packs.all)' | RAILS_ENV=development bundle exec rails c
-mv packwerk.png ../01-03_extracted_messy_middle.png
-find . -name "package_todo.yml" -exec basename {} \; -exec cat {} \; > ../01-03_extracted_messy_middle_package_todo.yml
-$interactive && open ../01-03_extracted_messy_middle.png
-
-find . -iname "account_actions_controller_spec.rb" | xargs rspec spec/features
