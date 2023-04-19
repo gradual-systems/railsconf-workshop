6dbc54b4eb233e2569930d219d4004ec69a073d3
Alex Evanczuk <alex.evanczuk@gusto.com>
2023-04-19 08:30:47 -0400

    Update 00 script so it is much simpler now that we do not need to boot app

--- 00.sh
+++ 00.sh
@@ -1,39 +1,6 @@
 set -e
 
-echo "Ensure Mastodon tests work"
+echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out branch XYZ"
 
-brew install gnu-sed libidn libpq postgresql redis yarn ffmpeg imagemagick graphviz
-
-
-export NVM_DIR=$HOME/.nvm;
-source $NVM_DIR/nvm.sh;
-nvm use 19 || nvm install 19 && nvm use 19 
-# Problems with this step?
-# If you get `Error: error:0308010C:digital envelope routines::unsupported`, 
-# then run `export NODE_OPTIONS=--openssl-legacy-provider`
-
-export PATH="$HOME/.rbenv/shims:$PATH"
-rbenv local 3.2.2 || rbenv install 3.2.2 && rbenv local 3.2.2 
-
+brew install gnu-sed graphviz
 bundle install
-
-# From https://github.com/mastodon/mastodon/blob/main/.devcontainer/post-create.sh
-
-yarn --frozen-lockfile
-
-git checkout -- Gemfile.lock
-
-RAILS_ENV=test ./bin/rails db:drop
-RAILS_ENV=test ./bin/rails db:setup
-
-RAILS_ENV=development ./bin/rails db:drop 
-RAILS_ENV=development ./bin/rails db:setup > /dev/null
-
-RAILS_ENV=development ./bin/rails assets:precompile
-RAILS_ENV=test ./bin/rails assets:precompile
-# Problems with this step?
-# Try running this instead
-# `cp public/packs/manifest.json public/packs-test/manifest.json`
-
-
-find . -iname "account_actions_controller_spec.rb" | xargs bundle exec rspec spec/features
