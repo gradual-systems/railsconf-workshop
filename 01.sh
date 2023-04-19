e440311d5e8ae5ee67475d1e34a45c2c5b365a7b
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-19 12:12:16 -0600

    Don’t move javascripts (or rather, move them back). They need more care

--- 01.sh
+++ 01.sh
@@ -5,5 +5,7 @@ echo "Create admin pack"
 
 bin/packs create packs/admin
 bin/packs move packs/admin app/*/admin 
+bin/packs move . packs/admin/app/javascript
+
 bin/packwerk update
 which dot && bin/packs visualize || echo "Graphviz not installed. Run 'brew install graphviz' if you want to generate dependency graphs"
--- 05.sh
+++ 05.sh
@@ -5,7 +5,7 @@ echo "Create user facing app pack"
 
 bin/packs create packs/user_facing_app
 bin/packs move packs/user_facing_app app
-# gsed -i "s/'app'/'packs', 'user_facing_app', 'app'/" packs/user_facing_app/app/helpers/branding_helper.rb
-# gsed -i "s/'app'/'packs', 'user_facing_app', 'app'/" packs/user_facing_app/app/validators/reaction_validator.rb
+bin/packs move . packs/user_facing_app/app/javascript
+
 bin/packwerk update
 which dot && bin/packs visualize || echo "Graphviz not installed. Run 'brew install graphviz' if you want to generate dependency graphs"
