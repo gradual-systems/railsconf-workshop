8e18f0f47109233c379ed3eb0274797b2e9d2d55
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-19 09:49:33 -0600

    Simplify setup with packwerk fork that doesn’t require the app

--- 00.sh
+++ 00.sh
@@ -2,5 +2,32 @@ set -e
 
 echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out branch XYZ"
 
-brew install gnu-sed graphviz
+
+echo "
+gem 'packs-rails', group: %w(development test production)
+
+gem 'use_packs', group: %w(development test)
+gem 'visualize_packwerk', group: %w(development test)
+
+# We're using this fork for the Mastodon Gradual Modularization workshop only. 
+# In a real-life situation you should replace the following line with commented out line below
+gem 'packwerk', github: 'gradual-systems/packwerk', branch: 'main', group: %w(development test)
+# gem 'packwerk'
+" >> Gemfile
+
 bundle install
+
+bundle binstubs bundler --force
+bundle binstub use_packs
+bundle binstub packwerk
+
+echo "pack_paths:
+  - packs/*
+  - .
+" > packs.yml
+
+echo "
+--require packs/rails/rspec
+" >> .rspec
+
+bin/packwerk init
