7a0d3aa8f49845cbd928484eec9ec23d34bec220
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-20 10:00:18 -0600

    layout improvements in 00

--- 00.sh
+++ 00.sh
@@ -2,7 +2,6 @@ set -e
 
 echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out branch XYZ"
 
-
 echo "
 gem 'packs-rails'
 
@@ -11,7 +10,9 @@ gem 'visualize_packwerk', group: %w(development test)
 
 # We're using this fork for the Mastodon Gradual Modularization workshop only. 
 # In a real-life situation you should replace the following line with commented out line below
-gem 'packwerk', github: 'gradual-systems/packwerk', branch: 'main', group: %w(development test)
+gem 'packwerk', 
+  github: 'gradual-systems/packwerk', 
+  branch: 'main', group: %w(development test)
 # gem 'packwerk', group: %w(development test)
 " >> Gemfile
 
