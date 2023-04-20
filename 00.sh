e9ae1ed21c4e5d5ab32bec86405b93a698c184f0
Stephan Hagemann <stephan.hagemann@gusto.com>
2023-04-20 10:08:13 -0600

    Correct reference to script 07

--- 00.sh
+++ 00.sh
@@ -1,6 +1,6 @@
 set -e
 
-echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out branch XYZ"
+echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out script 07"
 
 echo "
 gem 'packs-rails'
