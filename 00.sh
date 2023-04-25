59788622fbb4946985e502397bf5ea7e70f875da
Stephan Hagemann <stephan.hagemann@gmail.com>
2023-04-25 11:01:30 -0400

    Allow for more local-only script running

--- 00.sh
+++ 00.sh
@@ -2,21 +2,12 @@ set -e
 
 echo "Set up bare minimum for workshop. If you're interested in doing the advanced mode of this workshop (which involves setting up Mastodon so tests run successfully), check out script 07"
 
-echo "
-gem 'packs-rails'
+cat Gemfile | grep packwerk || bundle add packwerk --group 'development,test' --github 'gradual-systems/packwerk'
+cat Gemfile | grep packs-rails || bundle add packs-rails
+cat Gemfile | grep use_packs || bundle add use_packs --group 'development,test'
+cat Gemfile | grep visualize_packwerk || bundle add visualize_packwerk --group 'development,test'
 
-gem 'use_packs', group: %w(development test)
-gem 'visualize_packwerk', group: %w(development test)
-
-# We're using this fork for the Mastodon Gradual Modularization workshop only. 
-# In a real-life situation you should replace the following line with commented out line below
-gem 'packwerk', 
-  github: 'gradual-systems/packwerk', 
-  branch: 'main', group: %w(development test)
-# gem 'packwerk', group: %w(development test)
-" >> Gemfile
-
-bundle install
+bundle install --local || bundle install
 
 bundle binstubs bundler --force
 bundle binstub use_packs
@@ -27,8 +18,6 @@ echo "pack_paths:
   - .
 " > packs.yml
 
-echo "
---require packs/rails/rspec
-" >> .rspec
+cat .rspec | grep "packs/rails" || echo "--require packs/rails/rspec" >> .rspec
 
 bin/packwerk init
